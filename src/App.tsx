ea5e7493ec1e7417136e7fab2ecc4cc1fd015a29
RithvikNandhyala <nandhyala.r@northeastern.edu>
2025-03-28 01:00:20 -0400

    idek

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const HomePage: React.FC = () => {
     console.log("Searching for:", searchQuery);
 
     try {
-      const res = await fetch('https://citysimplify.com/chatbot', {
+      const res = await fetch('/chatbot', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
